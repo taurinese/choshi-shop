70974425ec9f6e58c7cc3eb3c2591b52a9b33eac
taurinese <enzodylan.taurines@gmail.com>
2020-06-11 13:45:18 +0200

    Add login/register form (only html/css) but doesnt work atm

--- views/userForm.php
+++ views/userForm.php
@@ -1,9 +1,11 @@
 <div id="user-page">
     <form action="index.php?controller=users&action=<?= $_GET['form'] ?>" method="post" id="user-form">
-        <h2>Inscription</h2>
-        <input type="text" name="first_name" id="first_name" placeholder="Prénom"><br>
-        <input type="text" name="last_name" id="last_name" placeholder="Nom de famille"><br>
-        <input type="text" name="address" id="address" placeholder="Adresse"><br>
+        <h3><?= $_GET['form'] == 'login'? "Connexion" : "Inscription" ?></h3>
+        <?php if($_GET['form'] == "register"): ?>
+            <input type="text" name="first_name" id="first_name" placeholder="Prénom"><br>
+            <input type="text" name="last_name" id="last_name" placeholder="Nom de famille"><br>
+            <input type="text" name="address" id="address" placeholder="Adresse"><br>
+        <?php endif; ?>
         <input type="email" name="user-email" id="user-email" placeholder="Email"><br>
         <input type="password" name="user-password" id="user-password" placeholder="Mot de passe"><br>
 
