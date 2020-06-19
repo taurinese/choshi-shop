421845c65cb167b9a65988cad313aed0e634897a
taurinese <enzodylan.taurines@gmail.com>
2020-06-19 16:51:43 +0200

    Homepage is responsive

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -1,43 +1,39 @@
 <div class="footer">
-    <div class="row">
-        <div>
-            <h5>Suivez-nous</h5>
-            <i class="fas fa-arrow-down icons text-transparent"></i>
-            <div class="flex-row-between">
-                <i class="fab fa-facebook-f icons"></i>
-                <i class="fab fa-instagram icons i-larger"></i>
-                <i class="fab fa-twitter icons i-larger"></i>
-            </div>
-        </div>
-        <div>
-            <h5>Infos livraison</h5>
-            <i class="fas fa-truck icons"></i>
-            <p>Livraison sous 72h</p>
-        </div>
-        <div>
-            <h5>Paiement sécurisé</h5>
-            <div>
-                <i class="fab fa-cc-visa icons"></i>
-                <i class="fab fa-cc-paypal icons"></i>
-            </div>
-            <p>Achetez sur notre site en toute confiance</p>
+    <div>
+        <h5>Suivez-nous</h5>
+        <i class="fas fa-arrow-down icons text-transparent"></i>
+        <div class="flex-row-between">
+            <i class="fab fa-facebook-f icons"></i>
+            <i class="fab fa-instagram icons i-larger"></i>
+            <i class="fab fa-twitter icons i-larger"></i>
         </div>
     </div>
-    <div class="row">
+    <div>
+        <h5>Infos livraison</h5>
+        <i class="fas fa-truck icons"></i>
+        <p>Livraison sous 72h</p>
+    </div>
+    <div>
+        <h5>Paiement sécurisé</h5>
         <div>
-            <h5>Contactez-nous</h5>
-            <i class="fas fa-at icons"></i>
-            <p>contact@choshi.com</p>
+            <i class="fab fa-cc-visa icons"></i>
+            <i class="fab fa-cc-paypal icons"></i>
         </div>
-        <div>
-            <h5>Retour & échange</h5>
-            <i class="fas fa-exchange-alt icons"></i>
-            <p>Retours acceptés sous 30 jours</p>
-        </div>
-        <div>
-            <h5>Newsletter</h5>
-            <input type="email" name="email" id="email" placeholder="example@email.com">
-            <input type="submit" value="S'inscrire">
-        </div>
+        <p>Achetez sur notre site en toute confiance</p>
+    </div>
+    <div>
+        <h5>Contactez-nous</h5>
+        <i class="fas fa-at icons"></i>
+        <p>contact@choshi.com</p>
+    </div>
+    <div>
+        <h5>Retour & échange</h5>
+        <i class="fas fa-exchange-alt icons"></i>
+        <p>Retours acceptés sous 30 jours</p>
+    </div>
+    <div>
+        <h5>Newsletter</h5>
+        <input type="email" name="email" id="email" placeholder="example@email.com">
+        <input type="submit" value="S'inscrire">
     </div>
 </div>
