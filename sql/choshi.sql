4fe40b7a0510ed1925c3626aee38cddac0510030
taurinese <enzodylan.taurines@gmail.com>
2020-06-21 00:10:05 +0200

    Added new SQL

--- sql/choshi.sql
+++ sql/choshi.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  sam. 20 juin 2020 à 16:56
+-- Généré le :  sam. 20 juin 2020 à 22:09
 -- Version du serveur :  10.4.10-MariaDB
 -- Version de PHP :  7.3.12
 
@@ -238,7 +238,7 @@ CREATE TABLE IF NOT EXISTS `products` (
   `quantity` int(11) NOT NULL,
   `is_displayed` tinyint(1) NOT NULL,
   `main_image` varchar(200) DEFAULT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `created_at` date NOT NULL DEFAULT current_timestamp(),
   `license_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `products_license_id` (`license_id`)
@@ -249,47 +249,47 @@ CREATE TABLE IF NOT EXISTS `products` (
 --
 
 INSERT INTO `products` (`id`, `name`, `price`, `description`, `quantity`, `is_displayed`, `main_image`, `created_at`, `license_id`) VALUES
-(1, 'Jiraya', 34, 'Figurine de Jiraya', 0, 1, '1.png', '2020-06-10 13:00:04', 3),
-(2, 'Livai', 35, 'Figurine de Livai : eseuinguise nguisen uigsenuignseuig nseiug niusengiusengiuseng uisneuignsei ugnsuiegnsiue ngiusenguisengu isneiugns euign siuengiuseng iusngi usngiusenui', 200, 1, '2.png', '2020-06-10 13:00:04', 4),
-(3, 'Erwin', 35, 'Figurine d\'Erwin', 198, 1, '3.png', '2020-06-10 13:00:04', 4),
-(4, 'One Piece Vol. 96', 12, 'One Piece Tome 96', 200, 1, '4.png', '2020-06-10 13:00:04', 2),
-(5, 'T-shirt Ramen', 35, 'T-shirt imprimé japonais', 200, 1, '5.png', '2020-06-10 13:00:04', NULL),
-(6, 'Luxus Draer', 30, 'Figurine de Luxus Draer', 200, 1, '6.png', '2020-06-14 13:00:04', NULL),
-(7, 'Sasuke', 30, 'Figurine de Sasuke', 200, 1, '7.png', '2020-06-14 13:00:04', 3),
-(8, 'Sagittarius', 30, 'Figurine de Sagittarius', 200, 1, '8.png', '2020-06-14 13:00:04', NULL),
-(9, 'Luffy Snake Man', 50.85, 'Figurine de Luffy Snake Man', 200, 1, '9.jpg', '2020-06-17 13:00:04', 2),
-(27, 'Cheetos', 5, 'Paquet de chips', 200, 1, '27.jpg', '2020-06-20 13:00:04', NULL),
-(28, 'Son Goku', 85, 'Figurine de Son Goku', 50, 1, '28.png', '2020-06-20 13:00:04', 6),
-(35, 'Dr Stone Vol 8', 14, 'Manga Dr Stone Volume 8', 200, 1, '35.jpg', '2020-06-20 13:00:04', NULL),
-(36, 'Cheetos BBQ', 5, 'Paquet de chips goût BBQ', 200, 1, '36.jpg', '2020-06-20 13:00:04', NULL),
-(37, 'Roronoa Zoro', 60, 'Figurine de Roronoa Zoro', 50, 1, '37.png', '2020-06-20 13:00:04', 2),
-(38, 'Kimono Fleurs', 39.99, 'Kimono imprimé fleurs', 50, 1, '38.jpg', '2020-06-20 13:00:04', NULL),
-(39, 'Kimono Démon', 39.99, 'Kimono imprimé démon', 50, 1, '39.jpg', '2020-06-20 13:00:04', NULL),
-(40, 'Coca-Cola Pêche', 3.49, 'Coca-Cola goût Pêche', 200, 1, '40.jpg', '2020-06-20 13:00:04', NULL),
-(41, 'Limonade ramune', 2.99, 'Limonade japonaise', 200, 1, '41.jpg', '2020-06-20 13:00:04', NULL),
-(42, 'Short Poketto', 25, 'Short Poketto', 50, 1, '42.jpg', '2020-06-20 13:00:04', NULL),
-(43, 'Hisoka', 40, 'Figurine de Hisoka', 200, 1, '43.png', '2020-06-20 13:00:04', NULL),
-(44, 'Thé vert', 4, 'Thé vert en canette', 200, 1, '44.jpg', '2020-06-20 13:00:04', NULL),
-(45, 'Dragon Ball Vol 1', 9.99, 'Dragon Ball Volume 1', 100, 1, '45.jpg', '2020-06-20 13:00:04', 6),
-(46, 'Dragon Ball Vol 2', 9.99, 'Dragon Ball Volume 2', 200, 1, '46.jpg', '2020-06-20 13:00:04', 6),
-(47, 'Death Note Vol 6', 8.99, 'Death Note Volume 6', 200, 1, '47.jpg', '2020-06-20 13:00:04', NULL),
-(48, 'Death Note Vol 13', 8.99, 'Death Note Volume 13', 100, 1, '48.jpg', '2020-06-20 13:00:04', NULL),
-(49, 'Pepsi Japan', 2.49, 'Pepsi Japan', 200, 1, '49.jpg', '2020-06-20 13:00:04', NULL),
-(50, 'T-shirt Akuma', 15, 'Tee-shirt Akuma', 100, 1, '50.jpg', '2020-06-20 13:00:04', NULL),
-(51, 'Cargo Shinobi', 39.99, 'Pantalon cargo shinobi', 50, 1, '51.jpg', '2020-06-20 13:00:04', NULL),
-(52, 'Fanta raisin', 3, 'Fanta goût raisin', 100, 1, '52.jpg', '2020-06-20 13:00:04', NULL),
-(53, 'MHA Vol 24', 10, 'My Hero Academia Volume 24', 200, 1, '53.jpg', '2020-06-20 13:00:04', 5),
-(54, 'MHA Vol 26', 10, 'My Hero Academia Volume 26', 200, 1, '54.jpg', '2020-06-20 13:00:04', 5),
-(55, 'MHA Vol 26', 10, 'My Hero Academia Volume 26', 200, 1, '55.jpg', '2020-06-20 13:00:04', 5),
-(56, 'One Piece Vol 92', 10.99, 'One Piece Volume 92', 200, 1, '56.jpg', '2020-06-20 13:00:04', 2),
-(57, 'One Piece Vol 95', 11.99, 'One Piece Volume 95', 200, 1, '57.jpg', '2020-06-20 13:00:04', 2),
-(58, 'Pop Corn Caramel Pêche', 8, 'Pop Corn Caramel Pêche', 100, 1, '58.jpg', '2020-06-20 13:00:04', NULL),
-(59, 'Veste Harajuku', 39.99, 'Veste Harajuku', 50, 1, '59.jpg', '2020-06-20 13:00:04', NULL),
-(60, 'Yukata Femme Bleu', 45, 'Yukata Femme Bleu', 100, 1, '60.jpg', '2020-06-20 13:00:04', NULL),
-(61, 'Yukata Femme Rouge', 45, 'Yukata Femme Rouge', 100, 1, '61.jpg', '2020-06-20 13:00:04', NULL),
-(62, 'T-shirt Katsuko', 15, 'Tee-shirt Katsuko', 200, 1, '62.jpg', '2020-06-20 13:00:04', NULL),
-(63, 'Salty Litchi', 5, 'Salty Litchi', 100, 1, '63.jpg', '2020-06-20 13:00:04', NULL),
-(64, 'Box surprise Juin', 30, 'Box surprise - spécial Juin', 20, 1, '64.jpg', '2020-06-20 13:00:04', NULL);
+(1, 'Jiraya', 34, 'Figurine de Jiraya', 0, 1, '1.png', '2020-06-10', 3),
+(2, 'Livai', 35, 'Figurine de Livai : eseuinguise nguisen uigsenuignseuig nseiug niusengiusengiuseng uisneuignsei ugnsuiegnsiue ngiusenguisengu isneiugns euign siuengiuseng iusngi usngiusenui', 200, 1, '2.png', '2020-06-10', 4),
+(3, 'Erwin', 35, 'Figurine d\'Erwin', 198, 1, '3.png', '2020-06-10', 4),
+(4, 'One Piece Vol. 96', 12, 'One Piece Tome 96', 200, 1, '4.png', '2020-06-10', 2),
+(5, 'T-shirt Ramen', 35, 'T-shirt imprimé japonais', 200, 1, '5.png', '2020-06-10', NULL),
+(6, 'Luxus Draer', 30, 'Figurine de Luxus Draer', 200, 1, '6.png', '2020-06-14', NULL),
+(7, 'Sasuke', 30, 'Figurine de Sasuke', 200, 1, '7.png', '2020-06-14', 3),
+(8, 'Sagittarius', 30, 'Figurine de Sagittarius', 200, 1, '8.png', '2020-06-14', NULL),
+(9, 'Luffy Snake Man', 50.85, 'Figurine de Luffy Snake Man', 200, 1, '9.jpg', '2020-06-17', 2),
+(27, 'Cheetos', 5, 'Paquet de chips', 200, 1, '27.jpg', '2020-06-20', NULL),
+(28, 'Son Goku', 85, 'Figurine de Son Goku', 50, 1, '28.png', '2020-06-20', 6),
+(35, 'Dr Stone Vol 8', 14, 'Manga Dr Stone Volume 8', 200, 1, '35.jpg', '2020-06-20', NULL),
+(36, 'Cheetos BBQ', 5, 'Paquet de chips goût BBQ', 200, 1, '36.jpg', '2020-06-20', NULL),
+(37, 'Roronoa Zoro', 60, 'Figurine de Roronoa Zoro', 50, 1, '37.png', '2020-06-20', 2),
+(38, 'Kimono Fleurs', 39.99, 'Kimono imprimé fleurs', 50, 1, '38.jpg', '2020-06-20', NULL),
+(39, 'Kimono Démon', 39.99, 'Kimono imprimé démon', 50, 1, '39.jpg', '2020-06-20', NULL),
+(40, 'Coca-Cola Pêche', 3.49, 'Coca-Cola goût Pêche', 200, 1, '40.jpg', '2020-06-20', NULL),
+(41, 'Limonade ramune', 2.99, 'Limonade japonaise', 200, 1, '41.jpg', '2020-06-20', NULL),
+(42, 'Short Poketto', 25, 'Short Poketto', 50, 1, '42.jpg', '2020-06-20', NULL),
+(43, 'Hisoka', 40, 'Figurine de Hisoka', 200, 1, '43.png', '2020-06-20', NULL),
+(44, 'Thé vert', 4, 'Thé vert en canette', 200, 1, '44.jpg', '2020-06-20', NULL),
+(45, 'Dragon Ball Vol 1', 9.99, 'Dragon Ball Volume 1', 100, 1, '45.jpg', '2020-06-20', 6),
+(46, 'Dragon Ball Vol 2', 9.99, 'Dragon Ball Volume 2', 200, 1, '46.jpg', '2020-06-20', 6),
+(47, 'Death Note Vol 6', 8.99, 'Death Note Volume 6', 200, 1, '47.jpg', '2020-06-20', NULL),
+(48, 'Death Note Vol 13', 8.99, 'Death Note Volume 13', 100, 1, '48.jpg', '2020-06-20', NULL),
+(49, 'Pepsi Japan', 2.49, 'Pepsi Japan', 200, 1, '49.jpg', '2020-06-20', NULL),
+(50, 'T-shirt Akuma', 15, 'Tee-shirt Akuma', 100, 1, '50.jpg', '2020-06-20', NULL),
+(51, 'Cargo Shinobi', 39.99, 'Pantalon cargo shinobi', 50, 1, '51.jpg', '2020-06-20', NULL),
+(52, 'Fanta raisin', 3, 'Fanta goût raisin', 100, 1, '52.jpg', '2020-06-20', NULL),
+(53, 'MHA Vol 24', 10, 'My Hero Academia Volume 24', 200, 1, '53.jpg', '2020-06-20', 5),
+(54, 'MHA Vol 26', 10, 'My Hero Academia Volume 26', 200, 1, '54.jpg', '2020-06-20', 5),
+(55, 'MHA Vol 26', 10, 'My Hero Academia Volume 26', 200, 1, '55.jpg', '2020-06-20', 5),
+(56, 'One Piece Vol 92', 10.99, 'One Piece Volume 92', 200, 1, '56.jpg', '2020-06-20', 2),
+(57, 'One Piece Vol 95', 11.99, 'One Piece Volume 95', 200, 1, '57.jpg', '2020-06-20', 2),
+(58, 'Pop Corn Caramel Pêche', 8, 'Pop Corn Caramel Pêche', 100, 1, '58.jpg', '2020-06-20', NULL),
+(59, 'Veste Harajuku', 39.99, 'Veste Harajuku', 50, 1, '59.jpg', '2020-06-20', NULL),
+(60, 'Yukata Femme Bleu', 45, 'Yukata Femme Bleu', 100, 1, '60.jpg', '2020-06-20', NULL),
+(61, 'Yukata Femme Rouge', 45, 'Yukata Femme Rouge', 100, 1, '61.jpg', '2020-06-20', NULL),
+(62, 'T-shirt Katsuko', 15, 'Tee-shirt Katsuko', 200, 1, '62.jpg', '2020-06-20', NULL),
+(63, 'Salty Litchi', 5, 'Salty Litchi', 100, 1, '63.jpg', '2020-06-20', NULL),
+(64, 'Box surprise Juin', 30, 'Box surprise - spécial Juin', 20, 1, '64.jpg', '2020-06-20', NULL);
 
 -- --------------------------------------------------------
 
@@ -304,19 +304,21 @@ CREATE TABLE IF NOT EXISTS `products_rates` (
   `user_id` int(11) NOT NULL,
   `rate` int(11) NOT NULL,
   `content` varchar(200) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `created_at` date NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`id`),
   KEY `products_rates_product_id` (`product_id`),
   KEY `products_rates_user_id` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Déchargement des données de la table `products_rates`
 --
 
 INSERT INTO `products_rates` (`id`, `product_id`, `user_id`, `rate`, `content`, `created_at`) VALUES
-(2, 3, 3, 1, 'Article bof', '2020-06-18 13:00:04'),
-(3, 2, 3, 4, 'eskj geskij gjesknguisenguiosenuigse', '2020-06-18 13:00:04');
+(2, 3, 3, 1, 'Article bof', '2020-06-18'),
+(3, 2, 3, 4, 'eskj geskij gjesknguisenguiosenuigse', '2020-06-18'),
+(4, 1, 17, 5, 'Ce produit est vraiment incroyable je suis super fan !', '2020-06-20'),
+(5, 2, 17, 1, 'Je n\'aime pas moi j\'aime le rock et la basse!', '2020-06-20');
 
 -- --------------------------------------------------------
 
@@ -356,7 +358,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `is_admin` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Déchargement des données de la table `users`
@@ -368,7 +370,12 @@ INSERT INTO `users` (`id`, `first_name`, `last_name`, `adresse`, `password`, `em
 (5, 'rgrdeg', 'egregre', 'ergerger', '098f6bcd4621d373cade4e832627b4f6', 'exemple@mail.com', 0),
 (7, 'Enzo', 'Oui', 'adresse bateau', '098f6bcd4621d373cade4e832627b4f6', 'test@test.com', 0),
 (8, 'User', 'Admin', 'Administrator', '21232f297a57a5a743894a0e4a801fc3', 'user@admin.com', 1),
-(9, 'Morgan', 'Mosta', 'Regular user address', 'ee11cbb19052e40b07aac0ca060c23ee', 'user@user.com', 0);
+(9, 'Morgan', 'Mosta', 'Regular user address', 'ee11cbb19052e40b07aac0ca060c23ee', 'user@user.com', 0),
+(10, 'egege', 'esgsegse', 'segse', '098f6bcd4621d373cade4e832627b4f6', 'e@mail.com', 0),
+(14, 'esgseges', 'segseges', 'gegegegege', 'b2a5abfeef9e36964281a31e17b57c97', 'ege@mail.com', 0),
+(15, 'segsegseg', 'segsegseg', 'gesgesges', 'b2a5abfeef9e36964281a31e17b57c97', 'gegeg@mail.com', 0),
+(16, 'sgsegesggse', 'gsegse', 'egseges', 'bf7dffde1bf23ebb5d9462dafcd857dd', 'egegege@mail.com', 0),
+(17, 'Maxibebou', 'Maxichou', 'Chez Maxinounours', 'ff00e77e8acfca03a57527372edd6635', 'maxichou@maxime.com', 0);
 
 --
 -- Contraintes pour les tables déchargées
