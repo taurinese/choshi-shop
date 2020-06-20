a63739f1a2bcb894fd603d6cd5da949130f7ea01
taurinese <enzodylan.taurines@gmail.com>
2020-06-20 18:13:16 +0200

    Multiple images are correctly managed now, you can add or delete them without problem.

--- admin/models/Product.php
+++ admin/models/Product.php
@@ -35,8 +35,6 @@ function addProduct($informations)
 		$result = updateProductImg(false, $productId);
 	}
 	if($result && !empty($_FILES['images']['tmp_name'][0])){
-		/* var_dump($result, $_FILES['images']['tmp_name'][0]);
-		die(); */
 		$result = addMultipleProductImg($productId);
 	}
     return $result;
@@ -47,14 +45,15 @@ function deleteProduct($productId)
 {
     $db = dbConnect();
 	
-	//ne pas oublier de supprimer le fichier lié s'il y en un
-	//avec la fonction unlink de PHP
+
 	$result = productImageExist($productId);
-	var_dump($result);
-	die();
 	if(!empty($result)){
-		foreach($result as $image){
-			unlink('../assets/images/products/' . $image) ;
+		unlink('../assets/img/products/' . $result['main_image']) ;
+		if(isset($result['images']) && !empty($result['images'])){
+			$images = explode(',', $result['images']);
+			foreach($images as $image){
+				unlink('../assets/img/products/alt/' . $image) ;
+			}
 		}
 	}
 	$query = $db->prepare('DELETE FROM products WHERE id = ?');
@@ -79,7 +78,7 @@ function updateProduct($id, $informations)
 	]);
 	if($result) $result = deleteProductCategories($id);
 	if($result) $result = addProductCategories($id, $informations['categories']); // ici
-	if($result && !empty($_FILES['image']['tmp_name'])) $result = updateProductImg(true, $id);
+	if($result && !empty($_FILES['main_image']['tmp_name'])) $result = updateProductImg(true, $id);
 	if($result && !empty($_FILES['images']['tmp_name'][0])){
 		$result = addMultipleProductImg($id);
 	}
@@ -91,17 +90,12 @@ function updateProductImg($fileMayExists = true, $productId)
     $db = dbConnect();
 	if($fileMayExists == true){
 		//Vérification si image déjà présente
-		$images = productImageExist($productId, $db);
+		$images = productImageExist($productId);
 		$fileMayExists = !empty($images) ? true : false;
 		//Si oui, suppression de l'ancienne image
 
-
-		// Vérifier unlink, possible que ça marche pas et qu'il faille GROUP_CONCAT sur la requête puis explode ce qu'on reçoit
 		if ($fileMayExists){
-            foreach ($images as  $image) {
-                unlink('../assets/images/products/' . $image) ;
-            }
-
+			unlink('../assets/images/products/' . $images['main_image']) ;
 			$fileMayExists = false;
 		} 
 	}
@@ -127,16 +121,16 @@ function updateProductImg($fileMayExists = true, $productId)
 function addMultipleProductImg($productId)
 {
 	$db = dbConnect();
+	$query = $db->prepare('SELECT COUNT(image) FROM images_products WHERE product_id = ?');
+	$query->execute([ $productId ]);
+	$nbImg = $query->fetchColumn();
 	$queryString = 'INSERT INTO images_products (product_id, image) VALUES ';
 	$queryValues = array();
-	/* $query = $db->prepare('SELECT COUNT(*) AS total FROM images_products WHERE product_id = ?');
-	$query->execute([ $productId ]);
-	$result = $query->fetchAll(); */
 	$allowed_extensions = array( 'jpg' , 'jpeg' , 'gif', 'png', 'jfif' );
 	foreach ($_FILES['images']['name'] as $key => $image) {
 		$my_file_extension = pathinfo( $image, PATHINFO_EXTENSION);
 		if (in_array($my_file_extension , $allowed_extensions)){
-			$new_file_name = $productId . '_' . $key . '.' . $my_file_extension ;
+			$new_file_name = $productId . '_' . $nbImg . '.' . $my_file_extension ;
 			$destination = '../assets/img/products/alt/' . $new_file_name;
 			$result = move_uploaded_file( $_FILES['images']['tmp_name'][$key], $destination);
 			if($result){
@@ -147,11 +141,8 @@ function addMultipleProductImg($productId)
 			}
 			
 		}
+		$nbImg ++;
 	}
-/* 	var_dump($queryString);
-	echo(' ');
-	var_dump($queryValues);
-	die(); */
 	$query = $db->prepare($queryString);
 	$result = $query->execute($queryValues);
 	return $result;
@@ -164,7 +155,7 @@ function productImageExist($id)
     $db = dbConnect();
 	$query = $db->prepare('SELECT p.main_image, GROUP_CONCAT(ip.image) AS images FROM products p LEFT JOIN images_products ip ON p.id = ip.product_id WHERE p.id = ?');
 	$query->execute([$id]);
-	return $query->fetchAll();
+	return $query->fetch();
 }
 
 
@@ -210,6 +201,6 @@ function deleteAltImg($imageId)
 	$result = $query->execute([
 		$imageId
 	]);
-	unlink('../assets/images/products/alt/' . $image);
+	unlink('../assets/img/products/alt/' . $image);
 	return $result;
 }
